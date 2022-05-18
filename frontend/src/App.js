4b4cb1fb21e220f4c8a372641c88356b72ed6657
ALINE <hoshinoaline@yahoo.com.br>
2022-05-18 17:04:41 -0300

    feat: create formfor the tasks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import './global.css';
+import './sidebar.css';
 
 
 function App() {
   return (
-    <h1>Tasks</h1>
+    <div>
+      <aside>
+        <strong>Suas tarefas</strong>
+        <form>
+          <div className="input-block">
+          <label htmlFor="title">Título da tarefa</label>
+          <input />
+          </div>
+          <div className="input-block">
+            <label htmlFor="nota">Tarefas</label>
+            <textarea></textarea>
+          </div>
+          <div className="input-block">
+            <label htmlFor="status">Status</label>
+            <select>
+    <option></option>
+            </select>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+    </div>
   );
 }
 
